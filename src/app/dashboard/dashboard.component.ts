c060632b08003f3c5aa14119280037930cd95ab6
Rodrigo Tavares <rodrigocmtavares@users.noreply.github.com>
2022-10-22 11:09:34 -0400

    6. Get data from a server

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
-
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
@@ -20,7 +18,6 @@ export class DashboardComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes.slice(1, 5))
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
-
 }
